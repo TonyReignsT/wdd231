2cd04136340e7e2007cf273498af0d2bcdbb79fd
TonyReignsT <antonyochieng00@gmail.com>
2025-07-24 21:22:36 +0300

    dynamically displayed companies' data on the browser

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,54 @@
+const cards = document.querySelector('#cards');
+
+const getCompanyData = async () => {
+    try {
+        const companies = await fetch('./data/members.json')
+        const companiesData = await companies.json();
+        // console.log(companiesData)
+        displayCard(companiesData.companies);
+    } catch(error) {
+        console.error("An error occured: ", error);
+    }
+     
+}
+
+// Building the Card
+const displayCard = (companies) => {
+    companies.forEach(company => {
+        let card = document.createElement('div');
+        let name = document.createElement('h3');
+        let tagLine = document.createElement('p');
+        let image = document.createElement('img');
+        let email = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('p');
+
+        // Card attribute
+        card.classList.add('company-card');
+
+        name.textContent = `${company.name}`;
+        tagLine.textContent = `${company.tagline}`;
+        email.innerHTML = `<b>Email: </b> ${company.email}`;
+        phone.innerHTML = `<b>Phone: </b> ${company.phone}`;
+        url.innerHTML = `<b>URL: </b> ${company.website}`;
+
+        //Creating image
+        image.setAttribute('src', company.image);
+        image.setAttribute('alt', company.name);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '340');
+        image.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(tagLine);
+        card.appendChild(image);
+        card.appendChild(email);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        cards.appendChild(card);
+
+    })
+};
+
+getCompanyData();
