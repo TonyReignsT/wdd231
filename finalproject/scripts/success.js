f90fa57d3ffd6952cade713bbc38f3bada4b2e72
TonyReignsT <antonyochieng00@gmail.com>
2025-08-16 09:52:52 +0300

    fixed the hamburger menu on success page.

--- finalproject/scripts/success.js
+++ finalproject/scripts/success.js
@@ -1,3 +1,22 @@
+// ======================
+// Navigation
+// ======================
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+// Close navigation when any nav link is clicked
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
 // Get all form data from the URL
 const details = new URLSearchParams(window.location.search);
 
